5f01c9313348a3a78181701d2c1c13105e33c70c
Sebastian Simarro <ssebastian210@gmail.com>
2021-11-14 10:07:49 -0300

    <init><layout> estructura con BS + readme

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center">Super Hero Challenge</h1>
+      <hr />
+      {/* grid equipo */}
+      <div class="row text-center">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+      <div class="row text-center">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div> //fin container
+  );
+}
+
+export default App;
